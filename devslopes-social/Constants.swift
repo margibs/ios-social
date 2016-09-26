b5867f616b0f66f85662dc579fc89e98d29b9706
margibs <margibs_hbb@yahoo.com>
2016-09-26 22:46:52 +0800

    Sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by margibs on 26/09/2016.
+//  Copyright © 2016 margibs. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
