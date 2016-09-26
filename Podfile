16704ed6a8b24522ada51e1537cc209ce20e4600
margibs <margibs_hbb@yahoo.com>
2016-09-26 17:23:26 +0800

    Completed setting up Firebase integration

---
+++ Podfile
@@ -0,0 +1,13 @@
+
+platform :ios, '9.0'
+
+target 'devslopes-social' do
+
+  use_frameworks!
+
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+
+end
---
+++ devslopes-social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-social
+//
+//  Created by margibs on 26/09/2016.
+//  Copyright © 2016 margibs. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
